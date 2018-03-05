92773f88bcfdffa3797defcd55dd4f9ef06608b0
unknown <marcos.martval@gmail.com>
2018-03-05 17:51:16 +0100

    gonzalo

---
+++ lanzapatata/Assets/Scripts/explosion_1.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion_1 : MonoBehaviour {
+	private Rigidbody rb;
+	private float pow = 50.0f;
+	private float radius = 6.0f;
+	private GameObject gb;
+	// Use this for initialization
+	void Start () {
+		this.rb = GetComponent<Rigidbody>();
+		this.gb = GetComponent<GameObject>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision c){
+		Vector3 positionOfExplosion = transform.position;
+		//c.GetComponent<Rigidbody>().AddForce (transform.position, ForceMode.Impulse);
+	}
+}
